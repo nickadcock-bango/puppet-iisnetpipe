c5a6657c63e25adfd4cdffa65ec4ae1785ae5949
Nick Adcock <nick.adcock@bango.com>
2017-08-22 15:17:06 +0100

    adding comments to example

--- examples/init.pp
+++ examples/init.pp
@@ -1,14 +1,37 @@
+# Adds, if not already exists, the net.pipe protocol to the webapplication's enabled protocols
+iisnetpipe::webapplication_enabledprotocol { $app:
+    website => $site,
+    webapplication => $app,
+    ensure => present,
+}
+
+# Removes, if exists, the net.pipe protocol from the webapplication's enabled protocols
 iisnetpipe::webapplication_enabledprotocol { $app:
     website => $site,
     webapplication => $app,
     ensure => absent,
 }
 
+# Adds, if not already exists, the net.pipe protocol to the websites's enabled protocols
+iisnetpipe::website_enabledprotocol { $app:
+    website => $site,
+    ensure => present,
+}
+
+# Removes, if exists, the net.pipe protocol from the websites's enabled protocols
 iisnetpipe::website_enabledprotocol { $app:
     website => $site,
     ensure => absent,
 }
 
+# Adds, if not already exists, a net.pipe binding to the website
+iisnetpipe::website_binding { $bindinginformation: 
+    website => $site,
+    bindinginformation => $bindinginformation,
+    ensure => present,
+}
+
+# Removes, if exists, a net.pipe binding from the website
 iisnetpipe::website_binding { $bindinginformation: 
     website => $site,
     bindinginformation => $bindinginformation,
